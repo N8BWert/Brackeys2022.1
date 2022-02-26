c5b94de898286779b5693f0ab34f6b3cfc68a7b2
DAlba-sudo <d.alba.work@gmail.com>
2022-02-26 11:46:47 -0500

    Added Gravity to the FPV

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed = 7f;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +24,19 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
