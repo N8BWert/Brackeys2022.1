80b10dee8382d64200b48230cbc8578cb812e7f1
Nathaniel Wert <N8.Wert.B@gmail.com>
2022-02-26 21:25:43 -0500

    Factory Level technically complete

--- Assets/Scripts/EndFactoryActivation.cs
+++ Assets/Scripts/EndFactoryActivation.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndFactoryActivation : MonoBehaviour
 {
     public GameObject endSceneObject;
+    public Text sceneText;
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
             endSceneObject.GetComponent<FactorySceneTransition>().canEnter = true;
+            sceneText.text = "hmm...if she's not here she must have just left...\nMaybe I can catch her on the way down...";
         }
     }
 }
